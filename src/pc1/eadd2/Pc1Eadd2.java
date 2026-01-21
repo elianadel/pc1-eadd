d4c2fc22b5563de1f99bec1fe4e2459e2e5d3c72
Eliana <elianadeleon009@gmail.com>
2026-01-20 20:50:07 -0600

    Computacion

--- src/pc1/eadd2/Pc1Eadd2.java
+++ src/pc1/eadd2/Pc1Eadd2.java
@@ -16,7 +16,7 @@ public class Pc1Eadd2 {
     public static void main(String[] args) {
         System.out.println("Hola mundo en java");
         System.out.println("Quinto");
-
+        System.out.println("Computacion");
     }
     
 }
