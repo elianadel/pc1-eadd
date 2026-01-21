76dc4dddafe7a5e94db6ef7a6ba0018274c84fb1
Eliana <elianadeleon009@gmail.com>
2026-01-20 20:49:06 -0600

    Quinto

--- src/pc1/eadd2/Pc1Eadd2.java
+++ src/pc1/eadd2/Pc1Eadd2.java
@@ -15,6 +15,7 @@ public class Pc1Eadd2 {
      */
     public static void main(String[] args) {
         System.out.println("Hola mundo en java");
+        System.out.println("Quinto");
 
     }
     
